fd06fad39e6f0cb39627c58fda0144bb6decdc79
Muhammad Syaiful Mu'min <143513644+Syaiful313@users.noreply.github.com>
2025-03-03 00:34:53 +0700

    refactor: optimalkan randomuser (#10)

--- src/components/TestimonialSection.tsx
+++ src/components/TestimonialSection.tsx
@@ -1,46 +1,120 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import { Testimonial } from "@/types/testimonial";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-interface Testimonial {
-  name: string;
-  position: string;
-  company: string;
-  quote: string;
-  imageSrc: string;
-}
+const defaultTestimonials: Testimonial[] = [
+  {
+    name: "Sarah Williams",
+    position: "CEO",
+    company: "TechPro Innovations",
+    quote:
+      "Socio Space is where dreams become reality. The collaborative atmosphere and top-notch amenities make it stand out.",
+    imageSrc: "/sarah-williams.jpg",
+  },
+  {
+    name: "John Anderson",
+    position: "Marketing Director",
+    company: "HealthHub Solutions",
+    quote:
+      "Socio Space is my second home. Flexible plans, great workspaces, and a supportive community have been crucial for my business.",
+    imageSrc: "/john-anderson.jpg",
+  },
+  {
+    name: "Emily Turner",
+    position: "Founder",
+    company: "EcoLiving Essentials",
+    quote:
+      "Socio Space, a game-changer for our team. Versatile spaces and networking opportunities have boosted productivity and innovation.",
+    imageSrc: "/emily-turner.jpg",
+  },
+];
 
 const TestimonialSection = () => {
-  const testimonials: Testimonial[] = [
-    {
-      name: "Sarah Williams",
-      position: "CEO",
-      company: "TechPro Innovations",
-      quote:
-        "Socio Space is where dreams become reality. The collaborative atmosphere and top-notch amenities make it stand out.",
-      imageSrc: "/sarah-williams.jpg", // Replace with actual image path
-    },
-    {
-      name: "John Anderson",
-      position: "Marketing Director",
-      company: "HealthHub Solutions",
-      quote:
-        "Socio Space is my second home. Flexible plans, great workspaces, and a supportive community have been crucial for my business.",
-      imageSrc: "/john-anderson.jpg", // Replace with actual image path
-    },
-    {
-      name: "Emily Turner",
-      position: "Founder",
-      company: "EcoLiving Essentials",
-      quote:
-        "Socio Space, a game-changer for our team. Versatile spaces and networking opportunities have boosted productivity and innovation.",
-      imageSrc: "/emily-turner.jpg", // Replace with actual image path
-    },
-  ];
+  const [testimonials, setTestimonials] =
+    useState<Testimonial[]>(defaultTestimonials);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(
+          "https://randomuser.me/api/?results=3",
+        );
+
+        const companies = [
+          "TechPro Innovations",
+          "HealthHub Solutions",
+          "EcoLiving Essentials",
+        ];
+        const positions = ["CEO", "Marketing Director", "Founder"];
+
+        const quotes = [
+          "Socio Space is where dreams become reality. The collaborative atmosphere and top-notch amenities make it stand out.",
+          "Socio Space is my second home. Flexible plans, great workspaces, and a supportive community have been crucial for my business.",
+          "Socio Space, a game-changer for our team. Versatile spaces and networking opportunities have boosted productivity and innovation.",
+        ];
+
+        const newTestimonials = response.data.results.map(
+          (user: any, index: number) => ({
+            name: `${user.name.first} ${user.name.last}`,
+            position: positions[index],
+            company: companies[index],
+            quote: quotes[index],
+            imageSrc: user.picture.large,
+          }),
+        );
+
+        setTestimonials(newTestimonials);
+        setIsLoaded(true);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+        setIsLoaded(true);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const TestimonialCard = ({ testimonial }: { testimonial: Testimonial }) => (
+    <Card className="flex flex-col">
+      <CardHeader className="flex items-center justify-center">
+        <p className="mt-4 italic text-muted-foreground">
+          &quot;{testimonial.quote}&quot;
+        </p>
+      </CardHeader>
+      <CardFooter className="mt-5 flex items-center justify-start">
+        <div>
+          <Avatar className="h-24 w-24">
+            <AvatarImage
+              src={testimonial.imageSrc}
+              alt={testimonial.name}
+              className="object-cover"
+            />
+            <AvatarFallback>
+              {testimonial.name
+                .split(" ")
+                .map((name) => name[0])
+                .join("")}
+            </AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="ml-4">
+          <div className="font-semibold">{testimonial.name}</div>
+          <div className="text-sm text-muted-foreground">
+            {testimonial.position} at {testimonial.company}
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
+  );
 
   return (
     <section className="bg-white py-36">
       <div className="container mx-auto">
-        <div className="mb-12 flex justify-between text-left">
+        <div className="mb-12 flex flex-col justify-between text-left md:flex-row">
           <div className="flex flex-col items-start">
             <h3 className="mb-4 text-lg font-light uppercase">
               Hear What Our Members Have to Say
@@ -60,36 +134,7 @@ const TestimonialSection = () => {
 
         <div className="grid gap-8 md:grid-cols-3">
           {testimonials.map((testimonial, index) => (
-            <Card key={index} className="flex flex-col">
-              <CardHeader className="flex items-center justify-center">
-                <p className="mt-4 italic text-muted-foreground">
-                  &quot;{testimonial.quote}&quot;
-                </p>
-              </CardHeader>
-              <CardFooter className="mt-5 flex items-center justify-start">
-                <div>
-                  <Avatar className="h-24 w-24">
-                    <AvatarImage
-                      src={testimonial.imageSrc}
-                      alt={testimonial.name}
-                      className="object-cover"
-                    />
-                    <AvatarFallback>
-                      {testimonial.name
-                        .split(" ")
-                        .map((name) => name[0])
-                        .join("")}
-                    </AvatarFallback>
-                  </Avatar>
-                </div>
-                <div className="">
-                  <div className="font-semibold">{testimonial.name}</div>
-                  <div className="text-sm text-muted-foreground">
-                    {testimonial.position} at {testimonial.company}
-                  </div>
-                </div>
-              </CardFooter>
-            </Card>
+            <TestimonialCard key={index} testimonial={testimonial} />
           ))}
         </div>
       </div>
